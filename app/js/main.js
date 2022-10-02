27b255a3b884f7ec52e75b6ddd622888e6b8b9c0
unknown <daviddya4yk@mail.ru>
2022-10-03 00:28:04 +0300

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,11 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 2000,
   });
+
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#cccccc",
+    rotedFill: "ffc35b",
+    readOnly: true,
+  });
 });
